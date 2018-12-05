c2dc0d36f2ba4cbf89384ead379a427350671674
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-12-05 15:33:19 -0500

    Add encoding

--- student/testCurl.php
+++ student/testCurl.php
@@ -2,10 +2,10 @@
 	// TODO: PHP file to send student input for grading
 	rawurldecode($_POST);
 	$testData = array('studentInput' => $_POST['studentInput'], 'username' => $_POST['username']);//, 'id' = $_POST['id']);
-	rawurlencode($testData);
+	$encodedTest = rawurlencode($testData);
 	$ch = curl_init("https://web.njit.edu/~bkw2/saveUngraded.php");
 	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedTest);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$result = curl_exec($ch);
 
