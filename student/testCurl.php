e0a9b5466ffd5cf9f1d8125301b155fc8e01de2e
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2018-12-05 15:59:34 -0500

    Fixed incorrect variable

--- student/testCurl.php
+++ student/testCurl.php
@@ -5,7 +5,7 @@
 	//$encodedTest = rawurlencode($testData);
 	$ch = curl_init("https://web.njit.edu/~bkw2/saveUngraded.php");
 	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedTest);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $testData);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$result = curl_exec($ch);
 
